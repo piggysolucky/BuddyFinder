3e9248b3795fed1d64fe9977fdac6e9705865591
piggysolucky <erinyan1992@gmail.com>
2015-09-06 00:26:34 -0400

    add the user store Data logic

--- MyApplication/app/src/main/java/com/buckeye/osu/yanyan/buddyfinder/UserlocalStore.java
+++ MyApplication/app/src/main/java/com/buckeye/osu/yanyan/buddyfinder/UserlocalStore.java
@@ -1,8 +1,62 @@
 package com.buckeye.osu.yanyan.buddyfinder;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by yanyan on 9/5/2015.
  */
-public class UserlocalStore {
-    
+public class UserLocalStore {
+    public static final String SP_NAME = "userDetails";
+    SharedPreferences userLocalDatabase;
+    public UserLocalStore(Context context){ // only activity can provide the context
+        // Mode file can only be accessed by the calling application
+        userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);  // Create one if doesn't exists
+    }
+
+    public void storeUserInfo(User user){
+        // user an editor and commit the changes
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putString("name", user.getUserID());
+        spEditor.putString("email", user.getEmail());
+        spEditor.putString("password", user.getPassword());
+        spEditor.commit();
+    }
+
+    public User getLoggedInUser() {
+        String name = userLocalDatabase.getString("name", "");
+        String email = userLocalDatabase.getString("email", "");
+        String password = userLocalDatabase.getString("password", "");
+
+        return new User(name, email, password);
+    }
+
+    public void setUserLogIn(boolean loggedIn){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("loggedIn", loggedIn);
+        spEditor.commit();
+    }
+
+    public boolean getUserLoggedIn(){
+       return userLocalDatabase.getBoolean("loggedIn", false);
+    }
+
+    public String getUserName() {
+        return userLocalDatabase.getString("name","");
+    }
+
+    public String getUserEmail() {
+        return userLocalDatabase.getString("email","");
+    }
+
+    public String getUserPassword() {
+        return userLocalDatabase.getString("password", "");
+    }
+
+    public void clearUserData(){
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.clear();
+        spEditor.commit();
+    }
+
 }
--- MyApplication/app/src/main/java/com/buckeye/osu/yanyan/buddyfinder/login.java
+++ MyApplication/app/src/main/java/com/buckeye/osu/yanyan/buddyfinder/login.java
@@ -13,10 +13,12 @@ public class LogIn extends ActionBarActivity implements View.OnClickListener {
     private EditText etUserId, etPassword;
     private TextView register;
     private Button logIn;
+    private UserLocalStore mUserLocalStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUserLocalStore = new UserLocalStore(this);
 
         etUserId = (EditText)findViewById(R.id.log_userID);
         etPassword = (EditText)findViewById(R.id.log_password);
@@ -28,9 +30,31 @@ public class LogIn extends ActionBarActivity implements View.OnClickListener {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        if (authenticate() == true){
+           displayUserDetail();
+        }
+    }
+
+    public void displayUserDetail() {
+        User user = mUserLocalStore.getLoggedInUser();
+
+        etUserId.setText(user.getUserID());
+        etPassword.setText("************");
+    }
+
+    public boolean authenticate() {
+        return mUserLocalStore.getUserLoggedIn();
+    }
+
+    @Override
     public void onClick(View v){
         switch(v.getId()){
             case R.id.login:
+                User user = new User(null, null, null);
+                mUserLocalStore.storeUserInfo(user);
+                mUserLocalStore.setUserLogIn(true);
                 break;
             case R.id.log_register:
                 startActivity(new Intent(this, Register.class));
